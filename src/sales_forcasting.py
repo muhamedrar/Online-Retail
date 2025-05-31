a6ad48c55d758397ea9a632d02366c5db582b889
Mohamed <mohameahmed0218@gmail.com>
2025-05-31 09:19:08 +0300

    add country filter

--- src/sales_forcasting.py
+++ src/sales_forcasting.py
@@ -23,22 +23,22 @@ order = tuple(map(int, config['ArimaForecasting']['order'].strip('()').split(','
 # Parse seasonal_order to support (2, 0, [1, 2], 7) format
 seasonal_order = ast.literal_eval(config['ArimaForecasting']['seasonal_order'])
 
-def prep_data_for_sales_forecasting(file_path, cluster=None,  rolling_window=rolling_window, decomposition_period=decomposition_period):
+def prep_data_for_sales_forecasting(file_path, cluster=None,country = None,  rolling_window=rolling_window, decomposition_period=decomposition_period):
 
     print(f"executing prep_data_for_sales_forecasting with file_path: {file_path}, rolling_window: {rolling_window}, decomposition_period: {decomposition_period}")
    
     if cluster is not None:
-        print("Loading data with clustering enabled.")
-          
-        df = load_data(file_path, with_cluster=True)
-        print(f"Data loaded with shape: {df.shape}")
+        df = load_data(file_path, with_cluster=True)    
         df = df[df['cluster'] == cluster].copy()
-        print(f"Data filtered for cluster {cluster}, new shape: {df.shape}")
+        if country is not None:
+            df = df[df['Country'] == country].copy()
+        
     
     else:
         print("Loading data with clustering disabled.")
         df = load_data(file_path, with_cluster=False)
-        
+        if country is not None:
+            df = df[df['Country'] == country].copy()
  
     print(f"before preprocessing, data shape: {df.shape}")
     df = preprocess_data(df)
@@ -86,10 +86,15 @@ class SalesForecaster:
         future_final_forecast = pd.Series(future_final_forecast.values, index=future_dates)
         return future_final_forecast 
     
-    def forcast_sepcific_cluster(filePath,cluster_id, future_steps=30):
-        
-        ClusterData = prep_data_for_sales_forecasting(filePath, cluster=cluster_id)
-        
+    
+    def fittingmodel_with_filtered_country(self, country,cluster):
+        trend, seasonal, resid = prep_data_for_sales_forecasting(file_path= data_clustered_path, cluster=cluster, country=country)
+        self.trend = trend.dropna()
+        self.seasonal = seasonal.dropna()
+        self.resid = resid.dropna()
+        self.model = ARIMA(self.resid, order=order, seasonal_order=seasonal_order)
+        self.fitted_model = self.model.fit()
+        ### not completed
         
 
     
